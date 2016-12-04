8652a62d1b3f8fc913f965418d0097cde9b5be8d
bwang8482 <wang940822@gmail.com>
2016-12-04 00:40:41 -0600

    finish decision tree with right print

--- bwang34_assign4/code/DecisionTree.py
+++ bwang34_assign4/code/DecisionTree.py
@@ -2,12 +2,10 @@ import sys
 import numpy as np
 from tree import *
 
-
 # python DecisionTree.py ../data/balance-scale.train ../data/balance-scale.test
 # python DecisionTree.py ../data/nursery.data.train ../data/nursery.data.test
 # python DecisionTree.py ../data/led.train.new ../data/led.test.new
 # python DecisionTree.py ../data/poker.train ../data/poker.test
-
 
 TRAIN = sys.argv[1]
 TEST = sys.argv[2]
@@ -30,18 +28,20 @@ def read_train():
 
 def read_train():
 	data = []
+	max_label = 0
 	with open(TRAIN, 'r') as train_file:
 		for line in train_file:
 			line = line.strip().split(' ')
 			record = []
 			label = line[0]
+			max_label = max(max_label, int(label))
 			record.append(label)
 			attrs = line[1:]
 			for attr in attrs:
 				index, value = attr.split(':')
 				record.append((index,value))
 			data.append(record)
-	return data
+	return data, max_label
 
 def read_test():
 	test = []
@@ -60,13 +60,18 @@ def read_test():
 			count += 1
 	return test, count
 
+def calculate_confusion_matrix(num, label_pair):
+	confusion_matrix = np.zeros((num, num))
+	for pair in label_pair:
+		confusion_matrix[int(pair[0])-1][int(pair[1])-1] += 1
+	return confusion_matrix
 
 if __name__ == '__main__':
-	data = read_train()
-	test, num = read_test()
+	data, class_num = read_train()
+	test, data_num = read_test()
 	decision_tree = tree(data)
 	correct_num, label_pair = decision_tree.test(test)
-	print 'accuracy: {}'.format(float(correct_num)/float(num))
-
-
+	confusion_matrix = calculate_confusion_matrix(class_num, label_pair)
+	print confusion_matrix
+	# print 'accuracy: {}'.format(float(correct_num)/float(data_num))
 
